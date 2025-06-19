1f6cbb63a723c82729ee5a3f8dd9fed2399e7d8c
Alex Adre <adre.alex.02@gmail.com>
2025-06-19 10:31:19 -0300

    agregar estilos

--- editar.php
+++ editar.php
@@ -31,6 +31,7 @@ if (!$producto){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Editar producto</title>
 </head>
 <body>
